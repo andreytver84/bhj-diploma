e8776326084744a4ace72b8d92e4a4a0328ed0af
andreytver84 <uvikmedium@mail.ru>
2022-02-20 18:51:59 +0300

    createRequest added to project

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,30 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest;
+    xhr.responseType = 'json';
+    try {
+        if (options.method === 'GET') {
+            let way = options.url + '?';
+            Object.entries(options.data).map(([key, value]) => way = way + key + '=' + value);
+            xhr.open('GET', way);
+            xhr.send();
+        } else {
+            const formData = new FormData;
+            Object.entries(options.data).map(([key, value]) => formData.append(key, value));
 
+            xhr.open('POST', options.url);
+            xhr.send(formData);
+        }
+        xhr.addEventListener('readystatechange', () => {
+            if (xhr.readystate && xhr.status === 200) {
+                options.callback(null, xhr.response);
+            };
+        });
+    }
+    catch (e) {
+        options.callback(e);
+    }
 };
+
+
